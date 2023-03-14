8b0825f1b6403def40dc2a6d19c2f463099ac775
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-03-13 22:24:27 -0300

    manipulando e exibindo a data de lancamento do item

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,8 +3,9 @@ import React from 'react';
 import './FeaturedMovie.css';
 
 export default ({item}) => {
-
     console.log(item);
+
+    let firstDate = new Date(item.first_air_date);
 
     return (
         <section className='featured' style={{
@@ -17,7 +18,7 @@ export default ({item}) => {
                     <div className='featured--name'>{item.original_name}</div>
                     <div className='featured--info'>
                         <div className='featured--points'>{(item.vote_average).toFixed(1)} pontos</div>
-                        <div className='featured--year'>2099</div>
+                        <div className='featured--year'>{firstDate.getFullYear()}</div>
                         <div className='featured--seasons'>{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
                     <div className='featuread--description'>{item.overview}</div>
